d01ed93e0e3f3b7465c51803496b431ac9a8f284
AMIT KUMAR MADINA <136113175+amitkumarmadina@users.noreply.github.com>
2024-12-19 14:03:54 +0000

    commit

--- CustomReact/customreact.js
+++ CustomReact/customreact.js
@@ -11,7 +11,7 @@ container.appendChild(domElement)
 const domElement=document.createElement(reactElement.type)
 domElement.innerHTML=reactElement.children
 for (const prop in reactElement.props) {
-    if(prop=='children') continue   ;
+    if(prop=='children'react) continue   ;
     domElement.setAttribute(prop,reactElement.props[prop])
     }container.appendChild(domElement)
 }
