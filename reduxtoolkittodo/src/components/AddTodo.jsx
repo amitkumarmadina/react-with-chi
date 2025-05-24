4708daf3f9744829942491c4bdc4b111823c1eb9
AMIT KUMAR MADINA <136113175+amitkumarmadina@users.noreply.github.com>
2025-05-24 18:36:50 +0000

    commit

--- reduxtoolkittodo/src/components/AddTodo.jsx
+++ reduxtoolkittodo/src/components/AddTodo.jsx
@@ -1,23 +1,17 @@
 import React , {useState} from 'react'
 import { useDispatch } from 'react-redux'
-import{} from '../features/todo/todoSlice'
+import{ addTodo } from '../features/todo/todoSlice'
 function AddTodo() {
 
     const[input, setInput] = useState('')
     const dispatch = useDispatch()
+    
     const addTodoHandler = (e) => {
         e.preventDefault()
         if(input.trim() === '') {
             return
         }
-        dispatch({
-            type: 'ADD_TODO',
-            payload: {
-                id: Math.random(),
-                title: input,
-                completed: false
-            }
-        })
+        dispatch(addTodo(input))
         setInput('')
     }
     
